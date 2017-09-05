4793658b6969daa06f1737b5dec809759e92dde9
erika <erickdaniels@live.com>
2017-09-05 11:02:50 -0500

    Initial commit

---
+++ app/src/main/java/com/i017114/tallernavigation/DosFragment.java
@@ -0,0 +1,31 @@
+package com.i017114.tallernavigation;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by reyes on 03/09/2017.
+ */
+
+public class DosFragment extends Fragment {
+
+
+    public DosFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dos, container, false);
+
+    }
+
+
+
+}
---
+++ app/src/main/java/com/i017114/tallernavigation/MainActivity.java
@@ -0,0 +1,68 @@
+package com.i017114.tallernavigation;
+
+
+import android.app.FragmentTransaction;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bnv = (BottomNavigationView) findViewById(R.id.bnv_1);
+        bnv.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.item_1:
+                        UnoFragment unoFragment = new UnoFragment();
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.fl_1, unoFragment)
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .addToBackStack(null)
+                                .commit();
+                        Toast.makeText(MainActivity.this, "ITEM 1", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.item_2:
+                        DosFragment dosFragment = new DosFragment();
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.fl_1, dosFragment)
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .addToBackStack(null)
+                                .commit();
+                        Toast.makeText(MainActivity.this, "ITEM 2", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.item_3:
+                        TresFragment tresFragment = new TresFragment();
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.fl_1, tresFragment)
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .addToBackStack(null)
+                                .commit();
+                        Toast.makeText(MainActivity.this, "ITEM 3", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu ,menu);
+        return true;
+    }
+}
+
